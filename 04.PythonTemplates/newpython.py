f43bc9e6010f804fd8cda525b02f1054a30f07c6
Danny Ding <34590506+danny8090@users.noreply.github.com>
2019-09-01 17:17:11 +0800

    01.Pycharm在创建py文件时,自动添加文件头注释

---
+++ 04.PythonTemplates/newpython.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+@author: danny ding
+@license: MIT
+@contact: goufaner520@outlook.com
+@software: Python
+@file: newpython.py
+@time: 2019/9/1 17:11
+@desc: 
+
+"""
