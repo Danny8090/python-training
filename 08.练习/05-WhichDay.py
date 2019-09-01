374802223709fd612ee13cead2b3b1d9170fc389
Danny Ding <34590506+danny8090@users.noreply.github.com>
2019-09-01 22:43:24 +0800

    计算指定的年月日是这一年的第几天调试

--- 08.练习/05-WhichDay.py
+++ 08.练习/05-WhichDay.py
@@ -30,6 +30,9 @@ def which_day(year, month, date):
         [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
         [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     ][is_leap_year(year)]
+
+    # print(is_leap_year(year))
+
     total = 0
     for index in range(month - 1):
         total += days_of_month[index]
